8e8b9b67f0879b352b74d52d7b8b82cc942ee0f8
blairecas <chaminealex@mail.ru>
2025-03-03 10:18:32 +0300

    fpga firmware for tang20k

--- 5_compile_videm.bat
+++ 5_compile_videm.bat
@@ -28,7 +28,7 @@ php -f ../scripts/lst2bin.php _videm2.lst ./release/videm2.sav sav
 if %ERRORLEVEL% NEQ 0 ( exit /b )
 
 del _videm2.mac
-del _videm2.lst
+rem del _videm2.lst
 
 ..\scripts\rt11dsk d .\release\neon.dsk .\release\videm2.sav >NUL
 ..\scripts\rt11dsk a .\release\neon.dsk .\release\videm2.sav >NUL
